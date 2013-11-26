006742f9ff2548049c31d6b45ebb535d84d34cca
marco90s <zhaoyali90@gmail.com>
2013-11-26 18:26:25 +0800

    首次提交到本地

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2013Äê11ÔÂ26ÈÕ
+ *      Author: Administrator
+ */
+
+#include<iostream>
+using namespace std;
+int days;
+int get_dayofweek();
+int get_year();
+int get_month(int);
+int main(){
+	int year,month,dayweek;
+	int leep_year;
+	char week[7][10]={"Saturday", "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"};
+	while (())
+}
+
+
